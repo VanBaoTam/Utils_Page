afe7441292ddacdf09ab2e3d9370e773fbb38c64
VanBaoTam <vanbaotam187@gmail.com>
2023-12-15 10:04:43 +0700

    add: middlewares

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -3,5 +3,5 @@ import { userRouter } from "./user.routes";
 import { dataRouter } from "./data.routes";
 export const routes = (app: Express) => {
   app.use("/v1/api/users", userRouter);
-  app.use("v1/api/data", dataRouter);
+  app.use("/v1/api/data", dataRouter);
 };
